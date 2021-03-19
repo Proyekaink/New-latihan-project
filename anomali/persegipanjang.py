3102da03869595633fc287f6bc2cd2bad684ef04
Rama Aditra <ramaaditra29@gmail.com>
2021-03-19 10:27:36 +0700

    Latihan penggunaan geometri

--- anomali/persegipanjang.py
+++ anomali/persegipanjang.py
@@ -1,2 +1,5 @@
+def info( ):
+    return 'Modul menghitung rumus persegipanjang'
+
 def hitung_luas_persegpanjang(p, l):
     return p * l
--- practice.py
+++ practice.py
@@ -1,5 +1,8 @@
-from anomali.persegipanjang import hitung_luas_persegpanjang
-from anomali.segitiga import hitung_luas_segitiga
+from anomali.persegipanjang import hitung_luas_persegpanjang, info as info_persegipanjang
+from anomali.segitiga import hitung_luas_segitiga, info as info_segitiga
 
+print(info_segitiga())
 print(f'hitung_luas_segitiga = {hitung_luas_segitiga(20, 60)}')
+
+print(info_persegipanjang())
 print(f'hitung_luas_persegipanjang = {hitung_luas_persegpanjang(20, 20)}')
